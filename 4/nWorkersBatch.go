effbfb8424c1b47d4f9fa7c5efe5b7a2d0f0597e
Stacie Sudie <staciesu@il-i4.msk.21-school.ru>
2024-10-04 16:32:45 +0300

    4th task finished

--- 4/nWorkersBatch.go
+++ 4/nWorkersBatch.go
@@ -1,7 +1,8 @@
 package main
-/* 
-	4. Реализовать постоянную запись данных в канал (главный поток). 
-	Реализовать набор из N воркеров, которые читают произвольные данные из канала и выводят в stdout. 
+
+/*
+	4. Реализовать постоянную запись данных в канал (главный поток).
+	Реализовать набор из N воркеров, которые читают произвольные данные из канала и выводят в stdout.
 	Необходима возможность выбора количества воркеров при старте.
 
 	====Не сделано===
@@ -12,31 +13,26 @@ package main
 
 */
 
-
-
 import (
 	"fmt"
-	"sync"
-	// "bufio"
 	"os"
-	"syscall"
 	"os/signal"
-	// "math/rand"
+	"sync"
+	"syscall"
 	"time"
 )
 
 func worker(id int, data <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for val := range data {
-		fmt.Printf("worker %d show us a random: %d\n", id, val)
-		time.Sleep(500 * time.Millisecond)
+		fmt.Printf("worker %d show us an integer: %d\n", id, val)
+		time.Sleep(100 * time.Millisecond)
 	}
-	fmt.Printf("worker %d finished", id)
+	fmt.Printf("worker %d finished\n", id)
 }
 
 func main() {
 	var wg sync.WaitGroup
-	// var mu sync.Mutex
 
 	workerCount := 0
 	maxBuffer := 100
@@ -44,9 +40,8 @@ func main() {
 	data := make(chan int, maxBuffer)
 	c := make(chan os.Signal, 1)
 
-
-    fmt.Print("Введите количество воркеров: ")
-    fmt.Scan(&workerCount)
+	fmt.Print("Введите количество воркеров: ")
+	fmt.Scan(&workerCount)
 	for w := 1; w <= workerCount; w++ {
 		wg.Add(1)
 		go worker(w, data, &wg)
@@ -57,26 +52,25 @@ func main() {
 	go func() {
 		<-c
 		signalReceived = true
-		fmt.Println("opened")
 		close(data)
-		fmt.Println("closed")
-		wg.Wait()
-		fmt.Println("Все горутинки отработали")
-		// mu.Unlock()
 	}()
-	/* 
+	/*
 		Вся проблема по записи в закрытый канал крылась тут. Внутри функции воркер, рэнжом идет пробег по каналу, но в сам канал значения записываются бесконечно, из-за этого рэнж цикл работает бесконечно, соответственно сигнал ctrl+c приходит в канал с, но ждет бесконечность времени, пока воркеры отработают, а они работают по бесконечно записывающему каналу. РЕШЕНИЕ: ДОБАВИТЬ УСЛОВИЕ В ЦИКЛ
 	*/
 	for !signalReceived {
-		time.Sleep(1000 * time.Millisecond)
-		fmt.Println("zapis do")
+		time.Sleep(300 * time.Millisecond) //запись в закрытый канал все равно будет если убрать здесь time sleep,
+		if signalReceived {                // это условие нужно т.к. в момент получение сигнала тело этого цикла уже может пройти проверку на !signalReceived и быть в отрабатывании такта, с помощью добавления этого if-a мы удостоверимся что в случае чего цикл не будет писать данные в закрытый канал.
+			break
+		}
 		data <- i
-		fmt.Println("zapis posle")
 		i++
 	}
+	wg.Wait()
+	fmt.Println("Все горутинки отработали")
+
 }
 
-/* 
+/*
 	Вывод:
 	Это была задача на воркер-пул
 
@@ -85,9 +79,7 @@ func main() {
 
 */
 
-
-
-/* 
+/*
 
 	ВЫВОД ОТ 03.10.2024
 	Я не понимаю почему программа себя так ведет. Вот Вывод
@@ -102,5 +94,12 @@ func main() {
 	exit status 2
 	====================================================================
 	Почему-то после присвоения signalReceived значения true, цикл все равно продолжает писать в канал, и закономерно ловит панику
-	Не разобрался: что такое паника 
+	Не разобрался: что такое паника
+*/
+
+
+/* 
+	ВЫВОД 04.10:
+	За эту задачу понял больше о каналах воркерпулах и вэйтгруппах чем за последние три месяца
+
 */

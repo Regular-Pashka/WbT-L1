77e9b70289f64cd632fcad1c59725d1e6d866aec
Stacie Sudie <staciesu@il-i4.msk.21-school.ru>
2024-10-11 22:25:16 +0300

    2 ways done

--- 6/gorutineExec.go
+++ 6/gorutineExec.go
@@ -1,45 +1,90 @@
 package main
-/* 
 
-	6. Реализовать все возможные способы остановки выполнения горутины. 
+/*
+
+	6. Реализовать все возможные способы остановки выполнения горутины.
 
 	ВОПРОС: Какие есть способы остановки выполнения горутины?
 	1. Через передачу сигнального канала
+	2. Через контекст 
+		2.1. withCancel
+		2.2. withTimeout
+		2.3. withDeadline
 
 */
 import (
+	"context"
 	"fmt"
 	"sync"
 )
 
+/*
+1. Способ
+func square(num int, signalChan chan bool, wg *sync.WaitGroup) {
 
-func multByTwo(id int, in <-chan int, out chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for val := range in {
-		out <- val * 2
-		fmt.Printf("worker %d doubled number %d", id, val)
+defer wg.Done()
+select {
+	case <-signalChan:
+		fmt.Printf("Goroutine stopped because of signal chan, num: %d\n",  num)
+		return
+	default:
 	}
-	fmt.Printf("worker %d finished his work", id)
+	result := num * num
+	fmt.Println(result)
+}
+*/
+
+func sumOfChan(ctx context.Context, in <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	sum := 0
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("gourutine was stopped by context")
+			return
+		case num, ok :=<- in:
+			if !ok {
+				fmt.Println("gourutine finished her work", sum)
+				return
+			}
+			sum += num
+		}
+	}
 }
 
+
 func main() {
-	var wg sync.WaitGroup
-	in := make(chan int, 30)
-	out := make(chan int, 30)
-	workerCount := 3
+
 	
+
+
+	// 2.1 WithCancel
+/* 
+	var wg sync.WaitGroup
+	c := make(chan int, 20)
+	ctx, cancel := context.WithCancel(context.Background())
+	wg.Add(1)
+	go sumOfChan(ctx, c, &wg)
 	for i := 0; i < 100; i++ {
-		in <- i
-	} //заполнение входного канала
-	for w := 0; w < workerCount; w++ {
-		wg.Add(1)
-		go multByTwo(w, in, out, &wg)
+		c <- i
 	}
-	for doubled := range out {
-		fmt.Print(doubled)
-	}
+	close(c)
+	cancel()
+	wg.Wait() */
+
+
+	// Через контекст
+
+	/*
+	1. Использование закрывающего канала 
+	
+	var wg sync.WaitGroup
+	num := 5
+	signalChan := make(chan bool)
+	wg.Add(1)
+	go square(num, signalChan, &mu , &wg);
+	signalChan <- false
 	wg.Wait()
-	close(in)
-	close(out)
-	fmt.Print("DONE")
+	 
+	*/
 }

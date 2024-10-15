ec66f99de5e97fe32ec69b755ac452a2a004574f
Stacie Sudie <staciesu@il-i4.msk.21-school.ru>
2024-10-15 22:43:37 +0300

    so strokami sueta...

--- 15/fixErr.go
+++ 15/fixErr.go
@@ -1,6 +1,6 @@
 package main
 
-/* 
+/*
 	К каким негативным последствиям может привести данный фрагмент кода, и как это исправить? Приведите корректный пример реализации.
 
 	var justString string
@@ -16,8 +16,24 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+// "fmt"
+
+var justString string
+
+func createHugeString(size int) string {
+	return string(make([]byte, size))
+}
+
+func someFunc() {
+	v := strings.Repeat("РОССИЯ", 100)//тут задается строка размером 1024 байта
+	fmt.Println(v)
+	justString = v[500:] // тут происходит копирование данных из среза байтов v в justString
+	fmt.Printf("\n\n%s", justString)
+}
+
 func main() {
-	
+	someFunc()
 }

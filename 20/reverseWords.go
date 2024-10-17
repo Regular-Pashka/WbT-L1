2ede36a21349e40e138afcee4b20c1a1a408555f
Stacie Sudie <staciesu@il-i4.msk.21-school.ru>
2024-10-17 22:39:25 +0300

    adapter in progress

--- 20/reverseWords.go
+++ 20/reverseWords.go
@@ -12,14 +12,14 @@ func reverseWords(s string) string {
 	split := strings.Split(s, " ")
 	length := len(split)
 	for i := 0; i < length / 2; i++ {
-		split[i], split[length - i -1] = split[length - i - 1], split[i]
+		split[i], split[length - i - 1] = split[length - i - 1], split[i]
 	}
-	result := split.
+	result := strings.Join(split, " ")
 	return result
 }
 
 func main() {
-	s := "snow dog sun"
+	s := "snow dog sun AAAA "
 	fmt.Println(s)
 	fmt.Println(reverseWords(s))
 }
